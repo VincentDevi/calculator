196311ba0269cb7f3336cb020203b335df8083e4
Devilers Vincent <vincent.devilers@gmail.com>
2022-08-02 22:25:35 +0200

    function getDiv added, for creating all the div with JS

--- script.js
+++ script.js
@@ -1,107 +1,107 @@
 
 
-const getDivision =() => {
-    const main = document.querySelector("main");
-    main.className = "calculator";
+// const getDivision =() => {
+//     const main = document.querySelector("main");
+//     main.className = "calculator";
 
-    const divScreen = document.createElement("div");
-    divScreen.className = "calculator__screen";
-    main.appendChild(divScreen);
+//     const divScreen = document.createElement("div");
+//     divScreen.className = "calculator__screen";
+//     main.appendChild(divScreen);
 
 
-    const divKeys = document.createElement("div");
-    divKeys.className = "keys";
-    main.appendChild(divKeys);
+//     const divKeys = document.createElement("div");
+//     divKeys.className = "keys";
+//     main.appendChild(divKeys);
 
-    const opop = document.createElement("div");
-    opop.className = "keys__operation"
-    divKeys.appendChild(opop);
+//     const opop = document.createElement("div");
+//     opop.className = "keys__operation"
+//     divKeys.appendChild(opop);
     
-    const divKeyNum = document.createElement("div");
-    divKeyNum.className = "keys__number";
-    divKeys.appendChild(divKeyNum);
+//     const divKeyNum = document.createElement("div");
+//     divKeyNum.className = "keys__number";
+//     divKeys.appendChild(divKeyNum);
 
-    for (let i=0; i<5; i++){
-        const divRow = document.createElement("div");
-        divRow.className = "keys__number__row";
-        divKeyNum.appendChild(divRow);
-    }
+//     for (let i=0; i<5; i++){
+//         const divRow = document.createElement("div");
+//         divRow.className = "keys__number__row";
+//         divKeyNum.appendChild(divRow);
+//     }
 
 
   
-}
+// }
 
 
 // function to give attribute button and  value to an element.
-const getAttribute = (element, index) =>{
-    element.setAttribute('type', 'button');
-    element.setAttribute('value', index);
-    return element
-}
+// const getAttribute = (element, index) =>{
+//     element.setAttribute('type', 'button');
+//     element.setAttribute('value', index);
+//     return element
+// }
 
-// fucntion to create Number, "." and "="
-const addButtonNumber = () =>{
-    const num = document.querySelector('.keys__number');
+// // fucntion to create Number, "." and "="
+// const addButtonNumber = () =>{
+//     const num = document.querySelector('.keys__number');
 
-    const row1List = ["(", ")", "%"];
-    for (const j of row1List) {
-        const but = document.createElement("input");
-        const attriElement = getAttribute(but, j);
-        let toAdd = num.children[0];
-        toAdd.appendChild(attriElement);
-    }
-    const row5List = ["0",".","="];
-    for (const h of row5List) {
-        const but = document.createElement("input");
-        const attriElement = getAttribute(but, h);
-        if (h==="."){
-            but.setAttribute("data_number",h);
+//     const row1List = ["(", ")", "%"];
+//     for (const j of row1List) {
+//         const but = document.createElement("input");
+//         const attriElement = getAttribute(but, j);
+//         let toAdd = num.children[0];
+//         toAdd.appendChild(attriElement);
+//     }
+//     const row5List = ["0",".","="];
+//     for (const h of row5List) {
+//         const but = document.createElement("input");
+//         const attriElement = getAttribute(but, h);
+//         if (h==="."){
+//             but.setAttribute("data_number",h);
 
-        }
-        else{
+//         }
+//         else{
 
-        }
-        let toAdd = num.children[4];
-        toAdd.appendChild(attriElement);
-    }
+//         }
+//         let toAdd = num.children[4];
+//         toAdd.appendChild(attriElement);
+//     }
 
 
-    for (let i =1; i<10;i++){
-        const but = document.createElement("input");
-        but.setAttribute("data_number",i);
-        const attriElement = getAttribute(but, i);
+//     for (let i =1; i<10;i++){
+//         const but = document.createElement("input");
+//         but.setAttribute("data_number",i);
+//         const attriElement = getAttribute(but, i);
 
-        if (i<4){
-            let toAdd = num.children[1];
-            toAdd.appendChild(attriElement);
+//         if (i<4){
+//             let toAdd = num.children[1];
+//             toAdd.appendChild(attriElement);
 
-        }
-        else if (i>3 && i<7) {
-            let toAdd = num.children[2];
-            toAdd.appendChild(attriElement);
+//         }
+//         else if (i>3 && i<7) {
+//             let toAdd = num.children[2];
+//             toAdd.appendChild(attriElement);
 
-        }
-        else{
-            let toAdd= num.children[3];
-            toAdd.appendChild(attriElement);
+//         }
+//         else{
+//             let toAdd= num.children[3];
+//             toAdd.appendChild(attriElement);
 
-        }
+//         }
         
         
-    }
+//     }
 
-}
+// }
 
-// function to create Operation
-const getOperation = () =>{
-    const operation = document.querySelector('.keys__operation')
-    const operationList = ['CE', '/', '*', '-', '+'];
-    for (let i =0;i<operationList.length;i++ ){
-        const ope = document.createElement("input");
-        const attriOperation = getAttribute(ope, operationList[i]);
-        operation.appendChild(attriOperation);
-    }
-}
+// // function to create Operation
+// const getOperation = () =>{
+//     const operation = document.querySelector('.keys__operation')
+//     const operationList = ['CE', '/', '*', '-', '+'];
+//     for (let i =0;i<operationList.length;i++ ){
+//         const ope = document.createElement("input");
+//         const attriOperation = getAttribute(ope, operationList[i]);
+//         operation.appendChild(attriOperation);
+//     }
+// }
 // getDivision();
 // addButtonNumber();
 // getOperation();
@@ -214,11 +214,23 @@ for (const aButton of buttonList) {
 
 }
 
-// to erase last element of String
-// .slice(0 , -1)
+const getDiv = () =>{
+    const divClassList = ['calculator__screen', 'keys', 'operation', 'keys__but', 'row'];
+    for (let i =0; i<divClassList.length; i++){
+        const nameClass = divClassList[i];
+        if(nameClass==="row"){
+            for (let j = 0; j < 4; j++){
+                const div = document.createElement('div');
+                div.className = nameClass;
+
+            }
+        }
+        else{
+            const div = document.createElement('div');
+            div.className = nameClass;
+        }
+    }
+}
 
 
-
-  const a = '1+9/2'
   
-  console.log(computeResult(a)) // Should display 10
